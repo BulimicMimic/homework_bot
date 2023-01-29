2cabe4fcefdddf3d0429c2fd9cfecedd10243fa0
Alexander Alatkin <usenoose@gmail.com>
2023-01-30 00:25:49 +0300

    to review 1.02

--- homework.py
+++ homework.py
@@ -7,10 +7,14 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-from exceptions import *
+from exceptions import (HomeworkBotException,
+                        UnavailableEndpointError,
+                        RequestEndpointError,
+                        InvalidResponseError,
+                        UnknownHomeworkStatusError,
+                        )
 
 load_dotenv()
-
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -19,7 +23,6 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
